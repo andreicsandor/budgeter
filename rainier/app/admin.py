8daa7c8522e710c5d6eeb0a6d58d1b6e40d042cd
Andrei-Cristian S <andrei.sandor@outlook.com>
2022-09-04 21:37:34 +0300

    Register models

--- rainier/app/admin.py
+++ rainier/app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Profile, Currency, Transaction, Type, Category 
+
 # Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(Currency)
+admin.site.register(Transaction)
+admin.site.register(Type)
+admin.site.register(Category)
