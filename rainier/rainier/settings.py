ba39972c62ce2ce6ab8e41d6648581f08f511bfe
Andrei-Cristian S <andrei.sandor@outlook.com>
2022-09-06 11:01:56 +0300

    Change CDNs to static files

--- rainier/rainier/settings.py
+++ rainier/rainier/settings.py
@@ -120,8 +120,6 @@ USE_THOUSAND_SEPARATOR = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
